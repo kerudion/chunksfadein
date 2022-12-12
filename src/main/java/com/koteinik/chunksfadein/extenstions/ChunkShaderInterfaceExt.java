d7ac5a65dafcd77686a7e2c8f1a8ce14227338ce
Koteinik <koteinikconsole@gmail.com>
2022-12-12 16:04:31 +0200

    Made main branch only to contain main information

--- src/main/java/com/koteinik/chunksfadein/extenstions/ChunkShaderInterfaceExt.java
+++
@@ -1,7 +0,0 @@
-package com.koteinik.chunksfadein.extenstions;
-
-import me.jellysquid.mods.sodium.client.gl.buffer.GlMutableBuffer;
-
-public interface ChunkShaderInterfaceExt {
-    public void setFadeDatas(GlMutableBuffer buffer);
-}
