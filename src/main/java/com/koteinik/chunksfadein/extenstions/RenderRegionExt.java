ac0af7ec054210486f0e2a06d19898e1d1530f27
Koteinik <koteinikconsole@gmail.com>
2022-12-27 23:17:46 +0200

    Optimized everything a bit more

--- src/main/java/com/koteinik/chunksfadein/extenstions/RenderRegionExt.java
+++ src/main/java/com/koteinik/chunksfadein/extenstions/RenderRegionExt.java
@@ -2,13 +2,11 @@ package com.koteinik.chunksfadein.extenstions;
 
 import java.util.List;
 
-import com.koteinik.chunksfadein.core.ChunkData;
-
 import me.jellysquid.mods.sodium.client.gl.device.CommandList;
 import me.jellysquid.mods.sodium.client.render.chunk.RenderSection;
 
 public interface RenderRegionExt {
     public void updateChunksFade(List<RenderSection> chunks, ChunkShaderInterfaceExt shader, CommandList commandList);
-    public ChunkData getChunkData(int x, int y, int z);
+    public float[] getChunkData(int x, int y, int z);
     public void completeChunkFade(int x, int y, int z);
 }
