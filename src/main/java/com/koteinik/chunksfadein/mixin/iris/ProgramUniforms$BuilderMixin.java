ce11cd70da8c5666f105b748899df985377d7251
Koteinik <koteinikconsole@gmail.com>
2023-01-07 20:05:56 +0200

    Fixed "Unsupported uniform" spam and shader logs

---
+++ src/main/java/com/koteinik/chunksfadein/mixin/iris/ProgramUniforms$BuilderMixin.java
@@ -0,0 +1,19 @@
+package com.koteinik.chunksfadein.mixin.iris;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.coderbot.iris.Iris;
+import net.coderbot.iris.gl.program.ProgramUniforms;
+
+@Pseudo
+@Mixin(value = ProgramUniforms.Builder.class)
+public class ProgramUniforms$BuilderMixin {
+    @Redirect(method = "buildUniforms", at = @At(value = "INVOKE", target = "Lnet/coderbot/iris/IrisLogging;warn(Ljava/lang/String;)V"), remap = false)
+    private void redirectUnsupportedUniformWarning(String warning) {
+        if (!warning.contains("Chunk_FadeDatas"))
+            Iris.logger.warn(warning);
+    }
+}
