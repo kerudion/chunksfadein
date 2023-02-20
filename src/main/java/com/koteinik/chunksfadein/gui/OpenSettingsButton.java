7f4a500bf65c23ee90b8bde6b98672a62fd30386
Koteinik <koteinikconsole@gmail.com>
2023-02-20 17:38:30 +0200

    Added a button to open settings menu if modmenu is not installed

---
+++ src/main/java/com/koteinik/chunksfadein/gui/OpenSettingsButton.java
@@ -0,0 +1,39 @@
+package com.koteinik.chunksfadein.gui;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.util.Identifier;
+
+public class OpenSettingsButton extends ButtonWidget {
+    private static Identifier MOD_ICON = new Identifier("chunksfadein", "icon.jpg");
+
+    private static final int buttonW = 20;
+    private static final int buttonH = 20;
+
+    public OpenSettingsButton(Screen parent, MinecraftClient client, int parentW, int parentH) {
+        super(parentW / 2 - 155 - buttonW - 5, parentH / 6 + 72 - 6, buttonH, buttonW, ScreenTexts.EMPTY,
+                new PressAction() {
+                    @Override
+                    public void onPress(ButtonWidget button) {
+                        client.setScreen(new SettingsScreen(parent));
+                    }
+                });
+    }
+
+    @Override
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.renderButton(matrices, mouseX, mouseY, delta);
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderTexture(0, MOD_ICON);
+
+        RenderSystem.enableDepthTest();
+        drawTexture(matrices, x + 1, y + 1, 0, 0, width - 2, height - 2, width - 2, height - 2);
+    }
+}
