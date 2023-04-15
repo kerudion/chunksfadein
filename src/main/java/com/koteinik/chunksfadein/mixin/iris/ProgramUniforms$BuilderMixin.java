3266503255fb0d57d54078dda103fea362afa253
Koteinik <koteinikconsole@gmail.com>
2023-04-15 15:09:19 +0300

    Fixed crash with Iris

--- src/main/java/com/koteinik/chunksfadein/mixin/iris/ProgramUniforms$BuilderMixin.java
+++ src/main/java/com/koteinik/chunksfadein/mixin/iris/ProgramUniforms$BuilderMixin.java
@@ -12,7 +12,7 @@ import net.coderbot.iris.gl.program.ProgramUniforms;
 @Pseudo
 @Mixin(value = ProgramUniforms.Builder.class)
 public class ProgramUniforms$BuilderMixin {
-    @Redirect(method = "buildUniforms", at = @At(value = "INVOKE", target = "Lnet/coderbot/iris/IrisLogging;warn(Ljava/lang/String;)V"), remap = false)
+    @Redirect(method = "buildUniforms", at = @At(value = "INVOKE", target = "Lnet/coderbot/iris/IrisLogging;warn(Ljava/lang/String;)V"), remap = false, require = 0)
     private void redirectUnsupportedUniformWarning(IrisLogging logging, String warning) {
         if (!warning.contains("Chunk_FadeDatas"))
             Iris.logger.warn(warning);
