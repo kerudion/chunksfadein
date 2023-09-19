0cd0ae4ebf0a905f3dca03a9e011ea7532692deb
kerudion <koteinikconsole@gmail.com>
2023-09-19 17:12:31 +0300

    v1.0.4-1.20.1: Fixed incompatibility with probably all shaders

---
+++ src/main/java/com/koteinik/chunksfadein/core/IrisOutputColorDeclaration.java
@@ -0,0 +1,13 @@
+package com.koteinik.chunksfadein.core;
+
+import io.github.douira.glsl_transformer.util.Type;
+
+public class IrisOutputColorDeclaration {
+    public final String name;
+    public final Type type;
+
+    public IrisOutputColorDeclaration(String name, Type type) {
+        this.name = name;
+        this.type = type;
+    }
+}
