816e73115c95324c378fd4a175a1f76e9cddc28e
kerudion <koteinikconsole@gmail.com>
2024-07-07 20:50:08 +0300

    Fixed settings screen title blurring

--- src/main/java/com/koteinik/chunksfadein/gui/SettingsScreen.java
+++ src/main/java/com/koteinik/chunksfadein/gui/SettingsScreen.java
@@ -111,9 +111,9 @@ public class SettingsScreen extends Screen {
     @Override
     public void render(DrawContext context, int mouseX, int mouseY, float delta) {
         renderBackground(context, mouseX, mouseY, delta);
+        super.render(context, mouseX, mouseY, delta);
         context.drawCenteredTextWithShadow(textRenderer, title, width / 2, height / 20,
                 16777215 | 255 << 24);
-        super.render(context, mouseX, mouseY, delta);
     }
 
     @Override
