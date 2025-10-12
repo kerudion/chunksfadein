d04a8f305bb73f78d0a976db64c86b09c3198f51
kerudion <koteinikconsole@gmail.com>
2025-10-12 18:50:04 +0300

    Fixed fading (again), fixed fading with DH

--- common/src/main/java/com/koteinik/chunksfadein/compat/dh/DHState.java
+++ common/src/main/java/com/koteinik/chunksfadein/compat/dh/DHState.java
@@ -12,7 +12,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class DHState {
 	public static final ThreadLocal<Long> sectionPosForCreatingBuffer = new ThreadLocal<>();
 
-	private static final Map<Vector2i, Fader> faders = new ConcurrentHashMap<>();
+	private static final Map<Byte, Map<Vector2i, Fader>> faders = new ConcurrentHashMap<>();
 	private static int lastLevel = 0;
 
 	public synchronized static Fader getFader(long pos) {
@@ -22,21 +22,37 @@ public class DHState {
 			faders.clear();
 		}
 
+		Map<Vector2i, Fader> faders = fadersAtLevel(detailLevel(pos));
+
 		Vector2i blockPos = blockPos(pos);
 		Fader fader = faders.get(blockPos);
+		if (fader != null) return fader;
 
-		if (fader == null)
-			populateDescendantFaders(pos, fader = new Fader(blockPos.x, blockPos.y));
+		populateDescendantFaders(
+			pos,
+			fader = new Fader(
+				(int) Math.floor(blockPos.x / 16.0),
+				(int) Math.floor(blockPos.y / 16.0)
+			)
+		);
 
 		return fader;
 	}
 
 	private static void populateDescendantFaders(long pos, Fader fader) {
-		faders.put(blockPos(pos), fader);
+		fadersAtLevel(detailLevel(pos)).put(blockPos(pos), fader);
 
-		if (DhSectionPos.getDetailLevel(pos) > 0)
+		if (detailLevel(pos) > 0)
 			for (int i = 0; i < 4; i++)
 				populateDescendantFaders(DhSectionPos.getChildByIndex(pos, i), fader);
+	}
+
+	private static Map<Vector2i, Fader> fadersAtLevel(byte level) {
+		return faders.computeIfAbsent(level, k -> new ConcurrentHashMap<>());
+	}
+
+	private static byte detailLevel(long pos) {
+		return DhSectionPos.getDetailLevel(pos);
 	}
 
 	private static Vector2i blockPos(long pos) {
