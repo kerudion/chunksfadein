25c89e4d590ef0127e4a89d550dcc365b6a6d5b8
kerudion <koteinikconsole@gmail.com>
2025-12-11 10:42:36 +0200

    Fixed some crashes

--- common/src/main/java/com/koteinik/chunksfadein/compat/sodium/mixin/ChunkShaderInterfaceMixin.java
+++ common/src/main/java/com/koteinik/chunksfadein/compat/sodium/mixin/ChunkShaderInterfaceMixin.java
@@ -5,8 +5,9 @@ import com.koteinik.chunksfadein.compat.sodium.ext.ChunkShaderInterfaceExt;
 import com.koteinik.chunksfadein.core.FadeShaderInterface;
 import com.koteinik.chunksfadein.core.SkyFBO;
 import com.koteinik.chunksfadein.hooks.CompatibilityHook;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.caffeinemc.mods.sodium.client.gl.buffer.GlMutableBuffer;
-import net.caffeinemc.mods.sodium.client.gl.shader.uniform.GlUniformInt;
+import net.caffeinemc.mods.sodium.client.gl.shader.uniform.*;
 import net.caffeinemc.mods.sodium.client.render.chunk.shader.ChunkShaderOptions;
 import net.caffeinemc.mods.sodium.client.render.chunk.shader.DefaultShaderInterface;
 import net.caffeinemc.mods.sodium.client.render.chunk.shader.ShaderBindingContext;
@@ -14,7 +15,10 @@ import org.lwjgl.opengl.GL13;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.IntFunction;
 
 @Mixin(value = DefaultShaderInterface.class, remap = false)
 public abstract class ChunkShaderInterfaceMixin implements ChunkShaderInterfaceExt {
@@ -30,6 +34,43 @@ public abstract class ChunkShaderInterfaceMixin implements ChunkShaderInterfaceE
 
 		fadeInterface = new FadeShaderInterface(context);
 		sky = context.bindUniformOptional("cfi_sky", GlUniformInt::new);
+	}
+
+	@Redirect(
+		method = "<init>",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/caffeinemc/mods/sodium/client/render/chunk/shader/ShaderBindingContext;bindUniform(Ljava/lang/String;Ljava/util/function/IntFunction;)Lnet/caffeinemc/mods/sodium/client/gl/shader/uniform/GlUniform;"
+		)
+	)
+	private GlUniform<?> redirectBinds(
+		ShaderBindingContext instance,
+		String s,
+		IntFunction<? extends GlUniform<?>> intFunction
+	) {
+		return instance.bindUniformOptional(s, intFunction);
+	}
+
+	@WrapWithCondition(
+		method = "setupState",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/caffeinemc/mods/sodium/client/gl/shader/uniform/GlUniformFloat;setFloat(F)V"
+		)
+	)
+	private boolean wrapSet(GlUniformFloat instance, float value) {
+		return instance != null;
+	}
+
+	@WrapWithCondition(
+		method = "setChunkData",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/caffeinemc/mods/sodium/client/gl/shader/uniform/GlUniformInt;set(Ljava/lang/Integer;)V"
+		)
+	)
+	private boolean wrapSet(GlUniformInt instance, Integer value) {
+		return instance != null;
 	}
 
 	@Override
