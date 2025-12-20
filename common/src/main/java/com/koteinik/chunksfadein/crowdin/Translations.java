f67a4bc30123115a2f74711f9393b64212f862b2
kerudion <koteinikconsole@gmail.com>
2025-12-20 23:08:14 +0200

    Fixed encoding bugs in translations

--- common/src/main/java/com/koteinik/chunksfadein/crowdin/Translations.java
+++ common/src/main/java/com/koteinik/chunksfadein/crowdin/Translations.java
@@ -12,7 +12,11 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedInputStream;
 import java.io.InputStream;
-import java.util.*;
+import java.nio.charset.StandardCharsets;
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
@@ -25,7 +29,7 @@ public class Translations {
 			.getClassLoader()
 			.getResourceAsStream("assets/chunksfadein/lang/en_us.json")) {
 			BufferedInputStream bis = new BufferedInputStream(Objects.requireNonNull(inputStream));
-			enUs = parseJsonTranslations(new String(bis.readAllBytes()));
+			enUs = parseJsonTranslations(new String(bis.readAllBytes(), StandardCharsets.UTF_8));
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
