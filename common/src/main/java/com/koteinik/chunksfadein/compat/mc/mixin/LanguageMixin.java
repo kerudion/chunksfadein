520dd9528b5e3cb8ec5573ce2352d5551550b2a9
kerudion <koteinikconsole@gmail.com>
2025-12-30 20:39:55 +0200

    Fixed "Toggle Fading" keybinding localization

---
+++ common/src/main/java/com/koteinik/chunksfadein/compat/mc/mixin/LanguageMixin.java
@@ -0,0 +1,28 @@
+package com.koteinik.chunksfadein.compat.mc.mixin;
+
+import com.koteinik.chunksfadein.crowdin.Translations;
+import net.minecraft.locale.Language;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = Language.class)
+public class LanguageMixin {
+	@Inject(
+		method = "getOrDefault(Ljava/lang/String;)Ljava/lang/String;",
+		cancellable = true,
+		at = @At(
+			value = "HEAD"
+		)
+	)
+	private void useTranslations(String id, CallbackInfoReturnable<String> cir) {
+		if (id.equals("key.category.chunksfadein.keybinds"))
+			id = "chunksfadein.keybinds";
+
+		if (!Translations.hasKey(id)) return;
+
+		cir.setReturnValue(Translations.resolve(id));
+		cir.cancel();
+	}
+}
