44b9ff726d3ce73ad3f9185bd478b5d18645073f
Moaead Yahya <moaead@gmail.com>
2026-01-21 07:25:35 +0000

    Fixed NPE crash when placing block entities
    When placing certain block entities (like doors from the Create mod),
    the getAnimationOffset() method can return null if the RenderSection
    for that block position is not yet available. This caused a
    NullPointerException in modifySubmitBlockEntities.
    
    Added a null check consistent with similar checks in EntityRendererMixin
    and ItemFrameEntityRendererMixin.
    
    Fixes #151

--- common/src/main/java/com/koteinik/chunksfadein/compat/mc/mixin/LevelRendererMixin.java
+++ common/src/main/java/com/koteinik/chunksfadein/compat/mc/mixin/LevelRendererMixin.java
@@ -115,6 +115,8 @@ public class LevelRendererMixin {
 			return;
 
 		float[] offset = ext.getAnimationOffset(state.blockPos.getCenter());
+		if (offset == null)
+			return;
 
 		matrices.translate(offset[0], offset[1], offset[2]);
 	}
