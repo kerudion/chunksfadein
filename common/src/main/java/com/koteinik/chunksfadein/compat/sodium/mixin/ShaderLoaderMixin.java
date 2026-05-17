d9557bb28abf376c5c817ad8053a675de565dc19
kerudion <koteinikconsole@gmail.com>
2026-05-17 18:07:21 +0300

    Fixed compatibility with Shine

--- common/src/main/java/com/koteinik/chunksfadein/compat/sodium/mixin/ShaderLoaderMixin.java
+++ common/src/main/java/com/koteinik/chunksfadein/compat/sodium/mixin/ShaderLoaderMixin.java
@@ -6,26 +6,24 @@ import com.koteinik.chunksfadein.core.FadeShader;
 import com.koteinik.chunksfadein.core.FogOverrideMode;
 import com.koteinik.chunksfadein.core.ShaderInjector;
 import com.koteinik.chunksfadein.hooks.CompatibilityHook;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.caffeinemc.mods.sodium.client.gl.shader.ShaderLoader;
 import net.minecraft.resources.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = ShaderLoader.class, remap = false)
 public abstract class ShaderLoaderMixin {
-	@Inject(method = "getShaderSource", at = @At("RETURN"), cancellable = true)
-	private static void modifyConstructor(Identifier name, CallbackInfoReturnable<String> cir) {
-		if (CompatibilityHook.isIrisShaderPackInUse())
-			return;
+	@WrapMethod(method = "getShaderSource")
+	private static String modifyConstructor(Identifier name, Operation<String> original) {
+		String source = original.call(name);
+		if (source == null || CompatibilityHook.isIrisShaderPackInUse())
+			return source;
 
 		String path = name.getPath();
 
 		String[] splittedPath = path.split("/");
 		String shaderFileName = splittedPath[splittedPath.length - 1];
-
-		String source = cir.getReturnValue();
 
 		switch (shaderFileName) {
 			case "fog.glsl":
@@ -44,7 +42,7 @@ public abstract class ShaderLoaderMixin {
 				break;
 		}
 
-		cir.setReturnValue(source);
+		return source;
 	}
 
 	private static ShaderInjector prepareFogInjector() {
