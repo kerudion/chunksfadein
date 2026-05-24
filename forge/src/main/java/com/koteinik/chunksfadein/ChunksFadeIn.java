2ba63ac23c86fa28256a1e347779df83d19a8183
kerudion <koteinikconsole@gmail.com>
2026-05-24 18:02:51 +0300

    Made it safe for FML servers

--- forge/src/main/java/com/koteinik/chunksfadein/ChunksFadeIn.java
+++ forge/src/main/java/com/koteinik/chunksfadein/ChunksFadeIn.java
@@ -6,9 +6,11 @@ import com.koteinik.chunksfadein.core.ModrinthApi;
 import com.koteinik.chunksfadein.crowdin.Translations;
 import com.koteinik.chunksfadein.gui.SettingsScreen;
 import net.minecraft.client.KeyMapping;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -21,8 +23,12 @@ public class ChunksFadeIn {
 	public static List<KeyMapping> KEYLIST = new ArrayList<>();
 
 	public ChunksFadeIn() {
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> ChunksFadeIn::init);
+	}
+
+	public static void init() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		bus.addListener(this::registerKeys);
+		bus.addListener(ChunksFadeIn::registerKeys);
 
 		Config.load();
 		ModrinthApi.load();
@@ -36,7 +42,7 @@ public class ChunksFadeIn {
 		);
 	}
 
-	public void registerKeys(RegisterKeyMappingsEvent event) {
+	public static void registerKeys(RegisterKeyMappingsEvent event) {
 		Keybinds.initKeybinds();
 		KEYLIST.forEach(event::register);
 		KEYLIST.clear();
