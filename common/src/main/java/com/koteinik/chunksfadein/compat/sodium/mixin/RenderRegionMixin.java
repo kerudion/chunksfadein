c71aac69bc1c05bb9547725ca10297201e36b5f8
kerudion <119808157+kerudion@users.noreply.github.com>
2026-08-14 00:32:59 +0300

    Fixed compatibility with Sodium 0.9.2-alpha.4

--- common/src/main/java/com/koteinik/chunksfadein/compat/sodium/mixin/RenderRegionMixin.java
+++ common/src/main/java/com/koteinik/chunksfadein/compat/sodium/mixin/RenderRegionMixin.java
@@ -1,22 +1,20 @@
 package com.koteinik.chunksfadein.compat.sodium.mixin;
 
+import com.koteinik.chunksfadein.compat.sodium.ChunkFadeInController;
+import com.koteinik.chunksfadein.compat.sodium.ext.ChunkShaderInterfaceExt;
+import com.koteinik.chunksfadein.compat.sodium.ext.RenderRegionExt;
+import com.koteinik.chunksfadein.compat.sodium.ext.RenderSectionExt;
+import net.caffeinemc.mods.sodium.client.gl.arena.ArenaAggregator;
+import net.caffeinemc.mods.sodium.client.gl.device.CommandList;
+import net.caffeinemc.mods.sodium.client.gl.device.GLRenderDevice;
 import net.caffeinemc.mods.sodium.client.render.chunk.RenderSection;
+import net.caffeinemc.mods.sodium.client.render.chunk.region.RenderRegion;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import com.koteinik.chunksfadein.compat.sodium.ChunkFadeInController;
-import com.koteinik.chunksfadein.compat.sodium.ext.ChunkShaderInterfaceExt;
-import com.koteinik.chunksfadein.compat.sodium.ext.RenderRegionExt;
-import com.koteinik.chunksfadein.compat.sodium.ext.RenderSectionExt;
-
-import net.caffeinemc.mods.sodium.client.gl.arena.staging.StagingBuffer;
-import net.caffeinemc.mods.sodium.client.gl.device.CommandList;
-import net.caffeinemc.mods.sodium.client.gl.device.GLRenderDevice;
-import net.caffeinemc.mods.sodium.client.render.chunk.region.RenderRegion;
 
 @Mixin(value = RenderRegion.class, remap = false)
 public class RenderRegionMixin implements RenderRegionExt {
@@ -26,7 +24,7 @@ public class RenderRegionMixin implements RenderRegionExt {
 	private ChunkFadeInController fadeController;
 
 	@Inject(method = "<init>", at = @At(value = "TAIL"))
-	private void modifyConstructor(int x, int y, int z, StagingBuffer stagingBuffer, CallbackInfo ci) {
+	private void modifyConstructor(int x, int y, int z, ArenaAggregator arenaAggregator, CallbackInfo ci) {
 		fadeController = new ChunkFadeInController(GLRenderDevice.INSTANCE.createCommandList());
 	}
 
